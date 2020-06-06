8d6ab4aff9efe6ba0f881ca76e3dc7347dd56510
chautruong79 <62908290+chautruong79@users.noreply.github.com>
2020-06-05 23:20:38 -0600

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,58 +2,25 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav
-      className="navbar navbar-expand-lg navbar-dark bg-primary"
-      style={{ backgroundColor: "violet" }}
-    >
-      <a className="navbar-brand" href="/">
-        Assignment Tracker
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarText"
-        aria-controls="navbarText"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+    <nav className="sticky-top b bt bb tc mw7 center mt4 black-80 avenir">
+      <a
+        className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l"
+        href="/"
       >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarText">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="/">
-              Assignments <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/saved">
-              Saved Assignments
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/create">
-              Create New Assignment
-            </a>
-          </li>
-        </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button
-            className="btn btn-outline-success  bg-primary "
-            style={{ color: "#36012e" }}
-            type="submit"
-          >
-            Search
-          </button>
-        </form>
-      </div>
+        Home
+      </a>
+      <a
+        className="f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l"
+        href="/saved"
+      >
+        Saved Assignments
+      </a>
+      <a
+        className="f6 f5-l link bg-animate black-80 hover-bg-light-blue dib pa3 ph4-l"
+        href="/create"
+      >
+        Create New Assignment
+      </a>
     </nav>
   );
 };
