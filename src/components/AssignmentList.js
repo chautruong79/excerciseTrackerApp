41c10bcc1586a8f43f4b041f7f5fe8bfd5dec662
chautruong79 <62908290+chautruong79@users.noreply.github.com>
2020-06-05 23:49:41 -0600

    Update AssignmentList.js

--- src/components/AssignmentList.js
+++ src/components/AssignmentList.js
@@ -2,19 +2,23 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Assignment from "./Assignment";
 
-const AssignmentList = () => {
+const AssignmentList = (props) => {
   const [assignments, setAssignments] = useState([]);
+  const path = props.match.path;
   useEffect(() => {
     axios
       .get("http://localhost:7000/assignments/")
       .then((res) => {
-        console.log(res.data);
-        setAssignments(res.data);
+        if (path === "/saved") {
+          setAssignments(res.data.filter((assignment) => assignment.isSaved));
+        } else {
+          setAssignments(res.data);
+        }
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [path]);
 
   const handleDelete = (id) => {
     axios.delete("http://localhost:7000/assignments/" + id).then((res) => {
@@ -29,40 +33,36 @@ const AssignmentList = () => {
     axios
       .patch("http://localhost:7000/assignments/edit/" + id, { isSaved: true })
       .then((res) => console.log(res.data));
+    // @ts-ignore
     window.location = "/saved";
+    console.log("click");
+  };
+
+  const handleEdit = (id) => {
+    window.location = "/edit/" + id;
   };
   return (
     <div>
-      <h3>Current Assignment</h3>
-      <table className="table">
-        <thead className="thead-light">
-          <tr>
-            <th>No</th>
-            <th>Subject</th>
-            <th>Content</th>
-            <th>Note</th>
-            <th>Due Date</th>
-            <th>Due In</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {assignments.map((assignment, ind) => (
-            <Assignment
-              key={assignment._id}
-              id={assignment._id}
-              no={ind + 1}
-              subject={assignment.subject}
-              content={assignment.content}
-              date={assignment.date}
-              note={assignment.note}
-              handleDelete={handleDelete}
-              handleSave={handleSave}
-              isSaved={assignment.isSaved}
-            />
-          ))}
-        </tbody>
-      </table>
+      <p className="heading">
+        {path === "/saved" ? "Saved" : "Current"} Assignments
+      </p>
+      <div className="main">
+        {assignments.map((assignment, ind) => (
+          <Assignment
+            key={assignment._id}
+            id={assignment._id}
+            no={ind + 1}
+            subject={assignment.subject}
+            content={assignment.content}
+            date={assignment.date}
+            note={assignment.note}
+            handleDelete={handleDelete}
+            handleSave={handleSave}
+            handleEdit={handleEdit}
+            isSaved={assignment.isSaved}
+          />
+        ))}
+      </div>
     </div>
   );
 };
